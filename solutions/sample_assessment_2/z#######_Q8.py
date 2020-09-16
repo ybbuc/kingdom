143d0561f886da07761479b083cb75d5fbdbf42d
Jakob Wells <jakobwithak@gmail.com>
2020-09-16 15:29:30 +0800

    Added a solution for question 8 in sample assessment 2

---
+++ solutions/sample_assessment_2/z#######_Q8.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+def draw_square(length):
+    forward(length)
+    left(90)
+    forward(length)
+    left(90)
+    forward(length)
+    left(90)
+    forward(length)
+    left(90)
+
+draw_square(150)
+
+forward(150)
+left(90)
+forward(150)
+right(90)
+
+draw_square(150)
