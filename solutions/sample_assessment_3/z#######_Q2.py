d3ab50d03ce63f8a65696e847e6aaf716332edfe
Jakob Wells <jakobwithak@gmail.com>
2020-09-16 22:30:45 +0800

    Added solution for question 2 of sample assessment 3

---
+++ solutions/sample_assessment_3/z#######_Q2.py
@@ -0,0 +1,17 @@
+def easy_peasy():
+    response = input('Easy peasy... ')
+    if response.lower() != 'lemon squeezy':
+        easy_peasy()
+
+
+def get_king_name():
+    king_name = input('Enter the name of the King: ')
+    if king_name.lower() != 'jakob':
+        print('%s is a false king!' % king_name)
+        get_king_name()
+
+
+easy_peasy()
+user_name = input('Enter your name: ')
+king_name = get_king_name()
+print('%s, you can be of good service to King Jakob.' % user_name)
